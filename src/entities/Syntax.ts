6dea60bda4598644232160820ec5ee814fafcb64
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-16 16:48:36 -0300

    Remove trim (constructor); implement check methods

--- src/entities/Syntax.ts
+++ src/entities/Syntax.ts
@@ -12,40 +12,133 @@ export type separator = {
 };
 
 export class Syntax {
-    public logicConstants: string[];
-    public propositionalSymbols: string[];
-    public logicOperators: logicOperator;
-    public separators: separator[];
-    
     constructor(
-        logicConstants: string[],
-        propositionalSymbols: string[],
-        logicOperators: logicOperator,
-        separators: separator[],
-        ) {
-        const logicConstantsTrim = logicConstants.map(constant => constant.trim());
+        public logicConstants: string[],
+        public propositionalSymbols: string[],
+        public logicOperators: logicOperator,
+        public separators: separator[],
+    ) {}
 
-        const logicOperatorsTrim = {
-            negativeOperator: logicOperators.negativeOperator.trim(),
-            andOperator: logicOperators.andOperator.trim(),
-            orOperator: logicOperators.orOperator.trim(),
-            implicationOperator: logicOperators.implicationOperator.trim(),
-            biImplicationOperator: logicOperators.biImplicationOperator.trim(),
-        };
+    isLogicConstant(expressionElement: string): Boolean
+    {
+        return this.logicConstants.includes(expressionElement);
+    }
 
-        const separatorsTrim = separators.map(({ first, last }: separator) => {
-            return { 
-                first: first.trim(), 
-                last: last.trim() 
-            };
+    isPropositionalSymbol(expressionElement: string): Boolean
+    {
+        return this.propositionalSymbols.includes(expressionElement);
+    }
+
+    isLogicOperator(expressionElement: string): Boolean
+    {
+        const matchedLogicOperators = Object.values(this.logicOperators).filter(symbol => symbol == expressionElement);
+
+        return matchedLogicOperators.includes(expressionElement);
+    }
+
+    isSeparator(expressionElement: string): Boolean
+    {
+        const matchedSeparators = this.separators.filter(separator => {
+            return separator.first == expressionElement || separator.last == expressionElement;
         });
 
-        
-        const propositionalSymbolsTrim = propositionalSymbols.map(symbol => symbol.trim());
-        
-        this.logicConstants = logicConstantsTrim;
-        this.propositionalSymbols = propositionalSymbolsTrim;
-        this.logicOperators = logicOperatorsTrim;
-        this.separators = separatorsTrim;
+        return matchedSeparators.length > 0;
+    }
+
+    isFirstSeparator(expressionElement: string): Boolean
+    {
+        const matchedSeparators = this.separators.filter(separator => {
+            return separator.first == expressionElement;
+        });
+
+        return matchedSeparators.length > 0;
+    }
+
+    isLastSeparator(expressionElement: string): Boolean
+    {
+        const matchedSeparators = this.separators.filter(separator => {
+            return separator.last == expressionElement;
+        });
+
+        return matchedSeparators.length > 0;
+    }
+
+    getMatchSeparator(separator: string): string|null
+    {
+        if (!this.isSeparator(separator)) {
+            return null;
+        }
+
+        if (this.isFirstSeparator(separator)) {
+            const matchedSeparators = this.separators.filter(separatorItem => {
+                return separatorItem.first == separator;
+            });
+
+            return matchedSeparators[0].last;
+        }
+
+        if (this.isLastSeparator(separator)) {
+            const matchedSeparators = this.separators.filter(separatorItem => {
+                return separatorItem.last == separator;
+            });
+
+            return matchedSeparators[0].first;
+        }
+
+        return null;
+    }
+
+    isLastSeparatorSyntaticallyValidOnExpression(previousIndex: number, expressionArray: string[], separator: string): Function|Boolean 
+    {
+        if (expressionArray[previousIndex] == this.getMatchSeparator(separator)) {
+            return true;
+        }
+
+        if (previousIndex < 0) {
+            return false;
+        }
+
+        return this.isLastSeparatorSyntaticallyValidOnExpression(previousIndex - 1, expressionArray, separator);
+    }
+
+    isFirstSeparatorSyntaticallyValidOnExpression(nextIndex: number, expressionArray: string[], separator: string): Function|Boolean 
+    {
+        if (expressionArray[nextIndex] == this.getMatchSeparator(separator)) {
+            return true;
+        }
+
+        if (nextIndex > expressionArray.length - 1) {
+            return false;
+        }
+
+        return this.isFirstSeparatorSyntaticallyValidOnExpression(nextIndex + 1, expressionArray, separator);
+    }
+
+    verifyPreviousItem(previousIndex: number, expressionArray: string[]): Boolean 
+    {
+        return (
+            this.isLogicConstant(expressionArray[previousIndex]) 
+            || this.isPropositionalSymbol(expressionArray[previousIndex])
+            || this.isSeparator(expressionArray[previousIndex])
+        );
+    }
+
+    verifyNextItem(nextIndex: number, expressionArray: string[]): Boolean 
+    {
+        return (
+            this.isLogicConstant(expressionArray[nextIndex]) 
+            || this.isPropositionalSymbol(expressionArray[nextIndex])
+            || this.logicOperators.negativeOperator == expressionArray[nextIndex]
+            || this.isSeparator(expressionArray[nextIndex])
+        );
+    }
+
+    verifyNextItemForNegativeOperator(nextIndex: number, expressionArray: string[]): Boolean 
+    {
+        return (
+            this.isLogicConstant(expressionArray[nextIndex]) 
+            || this.isPropositionalSymbol(expressionArray[nextIndex])
+            || this.isFirstSeparator(expressionArray[nextIndex])
+        );
     }
 }
