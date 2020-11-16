5ca233135dea24832ed71ec3fcf1ee18b04bc83e
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-16 17:17:07 -0300

    Implement AnalyzePropositionController

--- src/useCases/AnalyzeProposition/AnalyzePropositionController.ts
+++ src/useCases/AnalyzeProposition/AnalyzePropositionController.ts
@@ -1,5 +1,25 @@
+import { Request, Response } from "express";
+import { AnalyzePropositionUseCase } from "./AnalyzePropositionUseCase";
+
 export class AnalyzePropositionController {
-    async store() {
-        
+    constructor(
+        private analyzePropositionUseCase: AnalyzePropositionUseCase
+    ) {}
+
+    async store(request: Request, response: Response): Promise<Response> 
+    {
+        const { syntax, expression } = request.body;
+
+        try {
+            const result = this.analyzePropositionUseCase.execute({ syntax, expression });
+
+            return response.status(200).json({
+                result 
+            }).send();
+        } catch (error) {
+            return response.status(400).json({
+                message: error.message || 'Unexpected error.'
+            });
+        }
     }
 }
