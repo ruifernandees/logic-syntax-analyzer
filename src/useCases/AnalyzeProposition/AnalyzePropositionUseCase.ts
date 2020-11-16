19e663112f562ca888e019a6e8c0c89f88e077f1
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-16 16:50:10 -0300

    Implement AnalyzePropositionUseCase.execute

--- src/useCases/AnalyzeProposition/AnalyzePropositionUseCase.ts
+++ src/useCases/AnalyzeProposition/AnalyzePropositionUseCase.ts
@@ -12,8 +12,56 @@ export class AnalyzePropositionUseCase {
             separators
         } = data.syntax;
 
+        const expressionString = data.expression;
+        const expressionArray = expressionString.split(/\s+/);
+        const expressionStringWithoutBlankSpace = expressionArray.join();
+        const expression = expressionStringWithoutBlankSpace.split(",");
+
         const syntax = new Syntax(logicConstants, propositionalSymbols, logicOperators, separators);
 
-        
+        let allTruthyOfElements: Boolean[] = [];
+
+        expression.forEach((item, index) => {
+            if (syntax.isLogicOperator(item)) {
+                if (syntax.logicOperators.negativeOperator == item) {
+                    const isNextItemValid = syntax.verifyNextItemForNegativeOperator(index + 1, expressionArray);
+                    
+                    allTruthyOfElements.push(isNextItemValid);
+                    
+                    return;
+                }
+
+                const isPreviousItemValid = syntax.verifyPreviousItem(index - 1, expressionArray);
+                const isNextItemValid = syntax.verifyNextItem(index + 1, expressionArray);
+
+                allTruthyOfElements.push(isPreviousItemValid && isNextItemValid);
+
+                return;
+            }
+
+            if (syntax.isSeparator(item)) {
+                if (syntax.isFirstSeparator(item)) {
+                    const haveMatchSeparatorOnExpression = syntax.isFirstSeparatorSyntaticallyValidOnExpression(index + 1, expressionArray, item);
+                    
+                    allTruthyOfElements.push(haveMatchSeparatorOnExpression ? true : false);
+
+                    return;
+                }
+
+                if (syntax.isLastSeparator(item)) {
+                    const haveMatchSeparatorOnExpression = syntax.isLastSeparatorSyntaticallyValidOnExpression(index - 1, expressionArray, item);
+
+                    allTruthyOfElements.push(haveMatchSeparatorOnExpression ? true : false);
+
+                    return;
+                }
+
+                return;
+            }
+        });
+
+        const isExpressionValid = !allTruthyOfElements.includes(false);
+
+        return isExpressionValid;
     }
 }
