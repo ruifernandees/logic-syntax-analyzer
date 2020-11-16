cc23361fc1faa4dcec2f68fd43bd465d508605fb
ruifernandees <dev.ruifernandes@gmail.com>
2020-11-16 12:59:03 -0300

    Config paths of tsconfig.json to jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+const { compilerOptions } =  require('./tsconfig.json');
+const { pathsToModuleNameMapper } = require('ts-jest');
 /*
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/en/configuration.html
@@ -81,7 +83,7 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
